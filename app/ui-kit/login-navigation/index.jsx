d0386c66a67bddfe056cb8ad95ecfc25966a5d49
Yauheni Kokatau <ekokotov@gmail.com>
2019-09-02 00:43:18 +0300

    Safari 12 CSS fixes

--- app/ui-kit/login-navigation/index.jsx
+++ app/ui-kit/login-navigation/index.jsx
@@ -7,12 +7,12 @@ function Navigation() {
     <nav className="breadcrumb is-centered" aria-label="breadcrumbs">
       <ul>
         <li>
-          <NavLink to="/sign-in" activeClassName={styles.link__active}>
+          <NavLink to="/sign-in">
             Sign in
           </NavLink>
         </li>
         <li>
-          <NavLink to="/sign-up" activeClassName={styles.link__active}>
+          <NavLink to="/sign-up">
             Sign up
           </NavLink>
         </li>
